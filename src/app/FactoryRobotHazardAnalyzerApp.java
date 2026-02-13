4a9e806b307b554abdccbda18d427283936f212e
Devrajj-14 <dg9770@srmist.edu.in>
2026-02-13 14:50:16 +0530

    [Devraj]: UC1 - Create project skeleton, banner entry point, and gitignore

---
+++ src/app/FactoryRobotHazardAnalyzerApp.java
@@ -0,0 +1,10 @@
+package app;
+
+public class FactoryRobotHazardAnalyzerApp {
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println("        FACTORY ROBOT HAZARD ANALYZER   ");
+        System.out.println("========================================");
+        System.out.println("System initialized. (UC1)");
+    }
+}
